da9c1a3bef244de31773bfae7dcc799eb5f00427
Syed Mohammed <syedmohammedn22@gmail.com>
2021-11-24 11:27:24 +0530

    Change in post build script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,1 +1,13 @@
-echo "HELLLLOOOOO WORLD"
+#!/usr/bin/env bash
+
+brew tap wix/brew
+brew update
+brew install applesimutils
+
+# Change ios.sim.release to the detox configuration you want to run
+
+echo "Building the project for Detox tests..."
+npx detox build -c android.emu.debug
+
+echo "Executing Detox tests..."
+npx detox test --configuration android.emu.debug --cleanup
